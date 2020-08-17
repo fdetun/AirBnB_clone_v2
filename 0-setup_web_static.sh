bb60e452908b21c2e49dd263edb068f90330a209
fdetun <fouedads@gmail.com>
2020-08-18 00:06:01 +0100

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # task0
-f=/data/web_static/current/index.html
 sudo apt  update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/
@@ -9,10 +8,12 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf  /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo printf %s "server {
+sudo chown -R "$USER":"$USER" /etc/nginx/*
+sudo chmod -R 755 /etc/nginx/sites-available/default
+sudo chown -R "$USER":"$USER" /data/
+printf %s "server {
         listen 80 default_server;
         error_page 404 /404.html;
         location = /404.html {
@@ -24,5 +25,5 @@ sudo printf %s "server {
         location /current/ {
         alias  /data/web_static/current/hbnb_static;
         }
-}" > /etc/nginx/sites-available/default
+}" > sudo tee /etc/nginx/sites-available/default
 sudo service nginx restart
