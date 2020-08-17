3baaf13d6155ae1ace0e28e9d6f648c4f3453056
fdetun <fouedads@gmail.com>
2020-08-18 00:08:00 +0100

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,11 +8,11 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf  /data/web_static/releases/test/ /data/web_static/current
+sudo chown -h ubuntu:ubuntu /data/
 sudo chown -R "$USER":"$USER" /etc/nginx/*
 sudo chmod -R 755 /etc/nginx/sites-available/default
-sudo chown -R "$USER":"$USER" /data/
 printf %s "server {
         listen 80 default_server;
         error_page 404 /404.html;
@@ -25,5 +25,5 @@ printf %s "server {
         location /current/ {
         alias  /data/web_static/current/hbnb_static;
         }
-}" > sudo tee /etc/nginx/sites-available/default
+}" > /etc/nginx/sites-available/default
 sudo service nginx restart
