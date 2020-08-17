78582a2de04057061185b50780dc385531c3ee48
fdetun <fouedads@gmail.com>
2020-08-17 23:59:37 +0100

    0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# task0
+f=/data/web_static/current/index.html
+sudo apt  update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+if [ -f "$f" ]; then
+sudo rm -rf /data/web_static/current/*
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+else 
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+fi
+sudo printf %s "server {
+        listen 80 default_server;
+        error_page 404 /404.html;
+        location = /404.html {
+                root /usr/share/nginx/html;
+                internal;
+        }
+        location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}
+        location /current/ {
+        alias  /data/web_static/current/hbnb_static;
+        }
+}" > /etc/nginx/sites-available/default
+sudo service nginx restart
