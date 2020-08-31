f458ae0f48266bd9932a34305647e8b52a47437e
fdetun <fouedads@gmail.com>
2020-08-31 22:22:59 +0100

    1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""task 0"""
+from flask import Flask
+
+
+if __name__ == "__main__":
+    app = Flask(__name__)
+
+    @app.route("/", strict_slashes=False)
+    def home():
+        """home function"""
+        return "Hello HBNB!"
+
+    @app.route("/hbnb", strict_slashes=False)
+    def hbnb():
+        """home function"""
+        return "HBNB!"
+    app.run(host="0.0.0.0", port=5000)
