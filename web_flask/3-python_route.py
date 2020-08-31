c0b0046c0a0c5a1cf2bb0e755ee9cbc3cd0936f3
fdetun <fouedads@gmail.com>
2020-08-31 22:37:45 +0100

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""task 3"""
+from flask import Flask
+
+
+if __name__ == "__main__":
+    app = Flask(__name__)
+
+    @app.route("/", strict_slashes=False)
+    def home():
+        """the home directory displaying"""
+        return "Hello HBNB!"
+
+    @app.route("/hbnb", strict_slashes=False)
+    def hbnb():
+        """the hbnb directory displaying"""
+        return "HBNB!"
+
+    @app.route("/c/<string:f>", strict_slashes=False)
+    def cisfun(f):
+        """
+        the c directory displaying
+        replacing "_" by " "
+        """
+        return "C " + f.replace("_", " ")
+
+    @app.route("/python/<string:f>", strict_slashes=False)
+    def cisfun(f):
+        """
+        the python directory displaying
+        replacing "_" by " "
+        """
+        return "Python " + f.replace("_", " ")
+
+    app.run(host="0.0.0.0", port=5000)
