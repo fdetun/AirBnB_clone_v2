3f2b8a76fb944aa120a4fa9f46cc8c4c666b7df1
fdetun <fouedads@gmail.com>
2020-09-01 20:04:08 +0100

    6-number_odd_or_even.py

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""task 4"""
+from flask import Flask, render_template
+
+
+if __name__ == "__main__":
+    app = Flask(__name__)
+
+    @app.route("/", strict_slashes=False)
+    def home():
+        """the home directory displaying"""
+        return "Hello HBNB!"
+
+    @app.route("/hbnb", strict_slashes=False)
+    def hbnb():
+        """the hbnb directory displaying"""
+        return "HBNB!"
+
+    @app.route("/c/<string:f>", strict_slashes=False)
+    def cisfun(f):
+        """
+        the c directory displaying
+        replacing "_" by " "
+        """
+        return "C " + f.replace("_", " ")
+
+    @app.route("/python", strict_slashes=False)
+    @app.route("/python/<string:f>", strict_slashes=False)
+    def pythondisplay(f=None):
+        """
+        the python directory displaying
+        replacing "_" by " "
+        """
+        if f is None:
+            return "Python is cool"
+        else:
+            return "Python " + f.replace("_", " ")
+
+    @app.route("/number/<int:f>", strict_slashes=False)
+    def numberdisplay(f):
+        """
+        the c directory displaying
+        replacing "_" by " "
+        """
+        return "{:d} is a number".format(f)
+
+    @app.route("/number_template/<int:f>", strict_slashes=False)
+    def templdisplay(f):
+        """
+        the c directory displaying
+        replacing "_" by " "
+        """
+        return render_template("5-number.html", n=f)
+
+    @app.route("/number_odd_or_even/<int:f>", strict_slashes=False)
+    def oddevendisplay(f):
+        """
+        the c directory displaying
+        replacing "_" by " "
+        """
+        if f % 2 == 0:
+            s = "{} is even".format(f)
+            return render_template("6-number_odd_or_even.html", n=s)
+        else:
+            s = "{} is odd".format(f)
+            return render_template("6-number_odd_or_even.html", n=s)
+
+    app.run(host="0.0.0.0", port=5000)
