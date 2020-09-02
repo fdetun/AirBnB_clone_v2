10f7f87ccac3f23de55530c04899a46b0ec18f68
fdetun <fouedads@gmail.com>
2020-09-03 00:09:06 +0100

    pssss

--- models/base_model.py
+++ models/base_model.py
@@ -17,15 +17,14 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        #from models import storage
+        from models import storage
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if kwargs:
             for i, j in kwargs.items():
                 if i == "created_at" or i == "updated_at":
-                    j = datetime.strptime(kwargs['updated_at'], '\
-                    %Y-%m-%dT%H:%M:%S.%f')
+                    j = datetime.strptime(j, '%Y-%m-%dT%H:%M:%S.%f')
                 if i != "__class__":
                     setattr(self, i, j)
 
