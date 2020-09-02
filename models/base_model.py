7e43b48c297285a62d6c243ffc18e4200498e4c9
fdetun <fouedads@gmail.com>
2020-09-02 23:24:04 +0100

    shiiit

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        from models import storage
+        #from models import storage
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
